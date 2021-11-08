89173febaef98d1277017bc8e6d3da207584ffd9
pdutta <pdutta@harrier.cm.cluster>
2021-11-08 00:40:04 -0500

    TSS

--- preprocessing/.ipynb_checkpoints/pybedtools_genome_extraction-checkpoint.py
+++ preprocessing/.ipynb_checkpoints/pybedtools_genome_extraction-checkpoint.py
@@ -19,22 +19,61 @@ file_path= data_path+file_name
 gencode_gtf_file_path= gencode_path+ gtf_annotation_filename
 gencode_gff_file_path= gencode_path+ gff_annotation_filename
 reference_genome_file_path = reference_genome_path + reference_genome_name
+data_save_path= "/mnt/data05/shared/pdutta_data/Human_Genome_Data/region_wise_sequence"
 
 
-
+fasta = pybedtools.example_filename(reference_genome_file_path)
 df1 = read_gtf(gencode_gtf_file_path)
 print(df1.columns)
 print(df1.shape)
 
-
-a = pybedtools.BedTool(gencode_gff_file_path)
-fasta = pybedtools.example_filename(reference_genome_file_path)
+df_chr1 = df1[df1['seqname']=='chr1']
 
 
+df_transcript = pd.DataFrame(columns= ['transcript_id','gene_id','transcript_type','transcript_name','sequence'])
+df_exon = pd.DataFrame(columns= ['exon_id','exon_number','transcript_id','gene_id','transcript_type','gene_type','sequence'])
+df_gene = pd.DataFrame(columns= ['gene_id','gene_type','gene_name','sequence'])
+transcript_index=exon_index = gene_index= 0
+for index, row in df_chr1.iterrows():
+    #print ("###", index)
+    if (row['feature'] =="gene"):
+        df_gene.loc[gene_index, ['gene_id']]= row['gene_id']
+        df_gene.loc[gene_index, ['gene_type']]= row['gene_type']
+        df_gene.loc[gene_index, ['gene_name']]= row['gene_name']
+        query= row['seqname']+':'+str(row['start'])+'-'+str(row['end'])
+        print (index, query)
+        df_gene.loc[gene_index, ['sequence']] = BedTool.seq(query, fasta)
+        gene_index= gene_index+1
+    elif (row['feature'] =="transcript"):
+        df_transcript.loc[transcript_index, ['transcript_id']]= row['transcript_id']
+        df_transcript.loc[transcript_index, ['gene_id']]= row['gene_id']
+        df_transcript.loc[transcript_index, ['transcript_type']]= row['transcript_type']
+        df_transcript.loc[transcript_index, ['transcript_name']]= row['transcript_name']
+        query= row['seqname']+':'+str(row['start'])+'-'+str(row['end'])
+        print (index, query)
+        df_transcript.loc[transcript_index, ['sequence']] = BedTool.seq(query, fasta)
+        transcript_index= transcript_index+1
+    elif (row['feature'] =="exon"):
+        df_exon.loc[exon_index, ['exon_id']]= row['exon_id']
+        df_exon.loc[exon_index, ['exon_number']]= row['exon_number']
+        df_exon.loc[exon_index, ['transcript_id']]= row['transcript_id']
+        df_exon.loc[exon_index, ['gene_id']]= row['gene_id']
+        df_exon.loc[exon_index, ['transcript_type']]= row['transcript_type']
+        df_exon.loc[exon_index, ['gene_type']]= row['gene_type']
+        query= row['seqname']+':'+str(row['start'])+'-'+str(row['end'])
+        print (index, query)
+        df_exon.loc[exon_index, ['sequence']] = BedTool.seq(query, fasta)
+        exon_index= exon_index+1
 
-a = a.sequence(fi=fasta)
 
-print ("Crossed  the hurdle")
+df_exon.to_csv(data_save_path +"/"+ row['seqname']+"_exon_sequence.csv", index=False)
+df_transcript.to_csv(data_save_path +"/"+ row['seqname']+"_transcript_sequence.csv", index=False)
+df_gene.to_csv(data_save_path +"/"+ row['seqname']+"_gene_sequence.csv", index=False)
 
-print(open(a.seqfn).read())
+
+
+
+
+
+
 
