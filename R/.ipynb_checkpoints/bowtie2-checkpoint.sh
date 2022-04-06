1fabe5993db81e2fe341d2175bd786eea828e854
Dutta <pdutta@login01.cm.cluster>
2022-04-06 19:16:00 -0400

    Htseq

--- R/.ipynb_checkpoints/bowtie2-checkpoint.sh
+++ R/.ipynb_checkpoints/bowtie2-checkpoint.sh
@@ -3,5 +3,5 @@ FASTAQ_1=/home/pdutta/ncbi/fastq/SRR5448863_1.fastq
 FASTAQ_2=/home/pdutta/ncbi/fastq/SRR5448863_2.fastq
 OUT_DIR=/home/pdutta/ncbi/bowtie_output
 mkdir -p -- "$OUT_DIR"
-bowtie2 --no-unal -p 20 -x $GenomeIndexName -1 "$FASTAQ_1" -2 "$FASTAQ_2" -S "$OUT_DIR"/output.sam
+bowtie2 --no-unal -p 20 -x $GenomeIndexName -1 "$FASTAQ_1" -2 "$FASTAQ_2" -S "$OUT_DIR"/output.sam 2>&1 | tee "$OUT_DIR/bowtie_output.txt"
 #bowtie2 --no-unal -p n -x index_name -1 reads_1.fastq -2 reads_2.fastq -S output.sam
