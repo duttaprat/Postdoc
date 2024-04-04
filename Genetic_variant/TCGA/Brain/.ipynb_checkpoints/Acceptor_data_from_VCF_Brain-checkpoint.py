f7224c7e7a1afbf331b3f8585ae868e3eee00c57
Dutta <pdutta@login01.cm.cluster>
2024-04-04 14:33:40 -0400

    GDC_VCF_MANIFEST

---
+++ Genetic_variant/TCGA/Brain/.ipynb_checkpoints/Acceptor_data_from_VCF_Brain-checkpoint.py
@@ -0,0 +1,129 @@
+import os, io
+from collections import defaultdict
+import pandas as pd
+import pysam
+import pickle
+import argparse, sys, logging
+import pybedtools
+pybedtools.helpers.set_tempdir('/data/projects/temp')
+
+# In[2]:
+
+folder_name = sys.argv[1]
+parent_folder_path  = "/data/projects/GDC_Cancer_Wise/Brain/Data/SUB_VCF/"+ folder_name
+reference_file_path = "/data/projects/Resources/Gencode_genome_annotation/GRCh38.primary_assembly.genome.fa"
+
+
+reference_fasta = pysam.FastaFile(reference_file_path)
+df_acceptor_bed = pd.read_csv("/data/private/pdutta/DNABERT_data/Splice_sites/Acceptor_raw_files/Unique_Acceptor_RAW_Combined_BED.tsv", sep="\t")
+acceptor_bed = pybedtools.BedTool.from_dataframe(df_acceptor_bed)
+
+def get_vcf_gz_files_except_logs(root_folder):
+    all_files = []
+
+    # Walk through the directory tree
+    for dirpath, dirnames, filenames in os.walk(root_folder):
+        # If "logs" is in dirnames, remove it to avoid traversing it
+        if 'logs' in dirnames:
+            dirnames.remove('logs')
+
+        # Add only the filenames with the extension .vcf.gz in the current directory to the all_files list
+        for filename in filenames:
+            if filename.endswith('.vcf.gz'):
+                all_files.append(os.path.join(dirpath, filename))
+    return all_files
+
+
+# In[5]:
+
+
+def vcf_to_dataframe(vcf_path):
+    """
+    Convert a .vcf.gz file into a pandas DataFrame.
+
+    Parameters:
+    - vcf_path (str): path to the .vcf.gz file
+
+    Returns:
+    - pd.DataFrame: VCF data as a DataFrame
+    """
+
+    try:
+        # Open the VCF file
+        vcf_file = pysam.VariantFile(vcf_path)
+    except ValueError as e:
+        print(f"Failed to open file {vcf_path}: {e}")
+        with open("/data/projects/GDC_Cancer_Wise/Brain/Data/"+vcf_path.split("/")[-2]+"_error_log.txt", "a") as error_log:
+            error_log.write(f"{vcf_path}\n")
+        return None
+
+    # Extracting the data and the columns
+    data = []
+    
+    columns =  ["CHROM", "START_POS", "ID", "REF", "ALT", "QUAL", "FILTER"] + list(vcf_file.header.info.keys()) 
+    for record in vcf_file:
+        basic_data = [record.chrom, record.pos, record.id, record.ref, 
+                      ','.join(str(alt) for alt in record.alts), record.qual, record.filter.keys()[0] if record.filter.keys() else 'PASS']
+        row_data = [record.info.get(key) for key in vcf_file.header.info.keys()]
+        data.append(basic_data + row_data)
+
+    df = pd.DataFrame(data, columns=columns)
+    df["START_POS"] = df["START_POS"]-1
+    end = df["START_POS"] + df['REF'].str.len()
+    df.insert(2, 'END_POS', end)
+
+    # Close the VCF file
+    vcf_file.close()
+
+    return df
+
+
+# In[6]:
+
+
+files = get_vcf_gz_files_except_logs(parent_folder_path)
+intersected_vcf_data = {}
+df_statistics  = pd.DataFrame(columns= ['filename', 'gdc_tag', 'VCF_instance','VCF_feature','Intersected_instances', 'VCF_column_names' ])
+for file_path in files:
+    file_name =  file_path.split('/')[-1]
+    parts = file_name.split('_')
+    print(parts)
+    df_vcf = vcf_to_dataframe(file_path)
+    if df_vcf is None:
+        continue
+    vcf_bed = pybedtools.BedTool.from_dataframe(df_vcf)
+    intersect_vcf_acceptor = acceptor_bed.intersect(vcf_bed, wa=True, wb=True)
+    column_list  = df_acceptor_bed.columns.to_list() + df_vcf.columns.to_list()
+    df_intersection = intersect_vcf_acceptor.to_dataframe(names=column_list)
+    #print(df_intersection)
+    print(df_statistics)
+    #display(df_vcf)
+    gdc_tag = parts[-4]
+    instance =  df_vcf.shape[0]
+    feature = df_vcf.shape[1]
+    intersected_instance = df_intersection.shape[0]
+    feature_list = list(df_vcf.columns)
+    #print(file_name, gdc_tag, instance, feature,intersected_instance,  feature_list)
+    df_statistics.loc[len(df_statistics)] = [file_name, gdc_tag, instance, feature,intersected_instance,  feature_list]
+    intersected_vcf_data[file_name] = df_intersection
+
+
+
+
+
+output_path = "/data/projects/GDC_Cancer_Wise/Brain/Data/Preprocessed_file/" + folder_name
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
+
+# In[12]:
+
+
+df_statistics.to_csv(output_path+"/VCF_brain_statistics.tsv", sep="\t", index=False)
+
+
+# In[13]:
+
+
+with open(output_path+"/vcf_data.pkl", "wb") as file:
+    pickle.dump(intersected_vcf_data, file)
