2264096b7cddc630c7982d7c625b821445268da4
Dutta <pdutta@login01.cm.cluster>
2024-04-09 02:55:41 -0400

    VCF_brain

--- GDC_manifest/gdc.sh
+++ GDC_manifest/gdc.sh
@@ -3,10 +3,10 @@ chmod +x /home/pdutta/gdc-client
 export PATH=$PATH:/home/pdutta
 
 
-chmod 600 /home/pdutta/Data/Cancer_wiseGDC/GDC_token/gdc-user-token.2024-03-19T01_11_26.079Z.txt
+chmod 600 /home/pdutta/Data/Cancer_wiseGDC/GDC_token/gdc-user-token.2024-04-08T17_51_11.195Z.txt
 
 # gdc-client download -m /home/pdutta/Data/Cancer_wiseGDC/New_data/Brain/Manifest/gdc_manifest.2024-04-04_brain.txt -t /home/pdutta/Data/Cancer_wiseGDC/GDC_token/gdc-user-token.2024-03-19T01_11_26.079Z.txt -d /home/pdutta/Data/Cancer_wiseGDC/New_data/Lung/Downloaded_files/VCF/
 
-gdc-client download -m /home/pdutta/Data/Cancer_wiseGDC/New_data/Breast/Manifest/gdc_manifest.2024-04-04_breast.txt \
-                    -t /home/pdutta/Data/Cancer_wiseGDC/GDC_token/gdc-user-token.2024-03-19T01_11_26.079Z.txt \
-                    -d /home/pdutta/Data/Cancer_wiseGDC/New_data/Breast/Downloaded_files/VCF/
+gdc-client download -m /home/pdutta/Data/Cancer_wiseGDC/New_data/Breast/Manifest/gdc_manifest.2024-04-08_Breast.txt \
+                    -t /home/pdutta/Data/Cancer_wiseGDC/GDC_token/gdc-user-token.2024-04-08T17_51_11.195Z.txt \
+                    -d /home/pdutta/Data/Cancer_wiseGDC/New_data/Breast/Downloaded_files/VCF_NEW/
